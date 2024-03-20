6b2aec7e11264f387727c774286aaea0ddefe8b5
Tobias Nölle <tobias.noelle@icloud.com>
2024-03-20 20:12:53 +0100

    changed draw() function to implementate function requestAnimationFrame to render automaticly with possible fps and clean context befor render or draw other content

--- classes/world.class.js
+++ classes/world.class.js
@@ -5,14 +5,34 @@ class World {
         new Chicken(),
         new Chicken(),
     ]
+    canvas;
     ctx;
 
     constructor(canvas) {
         this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
         this.draw();        
     }
 
     draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
         this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+
+        this.chickens.forEach(chicken => {
+            this.ctx.drawImage(chicken.img, chicken.x, chicken.y, chicken.width, chicken.height);
+        });
+
+       /*  let pos = 100;
+        for (let i = 0; i < this.chickens.length; i++) {
+            const chicken = this.chickens[i];
+            this.ctx.drawImage(chicken.img, chicken.x + pos, chicken.y, chicken.width, chicken.height);
+            pos = pos + 100;
+        } */
+
+        let self = this;
+        requestAnimationFrame(function (){
+            self.draw();
+        });
     }
 }
