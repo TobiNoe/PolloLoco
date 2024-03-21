2f4f904d34efa0ea707c529beda987b3846d3653
Tobias Nölle <tobias.noelle@icloud.com>
2024-03-21 15:47:40 +0100

    change constructor to create random x values for every new Chicken()

--- classes/character.class.js
+++ classes/character.class.js
@@ -2,6 +2,8 @@ class Character extends MovableObject {
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
+        this.x = 50;
+        this.y = 180;
     }
 
     jump() {
