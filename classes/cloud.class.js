0472c27725e18047da19179052459c98dfe8fe40
Tobias Nölle <tobias.noelle@icloud.com>
2024-03-21 16:18:27 +0100

    changed space class Cloud

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -3,12 +3,11 @@ class Cloud extends MovableObject {
     width = 300;
     height = 200;
 
-    constructor(){
+    constructor() {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
         this.x = 0 + Math.random() * 500;
         this.y = 10;
         this.width = 300;
         this.height = 200;
     }
-
 }
