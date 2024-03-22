a1b8d310ed7459c2344e62dc734bd7453f0c1ae9
Tobias Nölle <tobias.noelle@icloud.com>
2024-03-22 20:57:00 +0100

    changed position x from character and chicken

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,9 +1,9 @@
 class Character extends MovableObject {
+    y = 178;
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
         this.x = 50;
-        this.y = 180;
     }
 
     jump() {
--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -1,9 +1,9 @@
 class Chicken extends MovableObject {
+    y = 350;
 
     constructor() {
         super().loadImage('./img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.x = 200 + Math.random() * 500;
-        this.y = 350;
         this.width = 75;
         this.height = 75;
     }
