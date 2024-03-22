00b6b5bfe9c57ff7377e8bf8a1d59c95447303e6
Tobias Nölle <tobias.noelle@icloud.com>
2024-03-22 20:42:39 +0100

    changed class layerPject.class to calculate y coordinate

--- classes/layerObject.class.js
+++ classes/layerObject.class.js
@@ -3,9 +3,10 @@ class LayerObject extends MovableObject {
     height = 480;
     
     
-    constructor(src, x, y){
+    constructor(src, x){
         super().loadImage(src);
         this.x = x;
-        this.y = y;
+        //man kann die y-koordinate berechenen. 480 ist die Höhe des canvas - die Höhe des Objektes
+        this.y = 480 - this.height;
     }
 }
--- classes/world.class.js
+++ classes/world.class.js
@@ -1,9 +1,9 @@
 class World {
     layers = [
-        new LayerObject('./img/5_background/layers/air.png', 0, 0),
-        new LayerObject('./img/5_background/layers/3_third_layer/1.png', 0, 0),
-        new LayerObject('./img/5_background/layers/2_second_layer/1.png', 0, 0),
-        new LayerObject('./img/5_background/layers/1_first_layer/1.png', 0, 0)
+        new LayerObject('./img/5_background/layers/air.png', 0),
+        new LayerObject('./img/5_background/layers/3_third_layer/1.png', 0),
+        new LayerObject('./img/5_background/layers/2_second_layer/1.png', 0),
+        new LayerObject('./img/5_background/layers/1_first_layer/1.png', 0)
     ];
     character = new Character();
     chickens = [
