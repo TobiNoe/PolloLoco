427a0901d08b32e0e20c72023ff5b4c55e3e8dad
Tobias Nölle <tobias.noelle@icloud.com>
2024-03-23 11:21:04 +0100

    added function moveClouds

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -1,13 +1,24 @@
 class Cloud extends MovableObject {
-    y = 10;
+    y = 20;
+    move;
     width = 300;
     height = 200;
 
     constructor() {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
         this.x = 0 + Math.random() * 500;
-        this.y = 10;
+        this.move = Math.random() * 0.25;
+        console.log(this.move);
+        this.moveClouds(this.move);
+        /* this.y = 10;
         this.width = 300;
-        this.height = 200;
+        this.height = 200; */
+    }
+
+    moveClouds() {
+        setInterval(() => {
+            this.x -= this.move;            
+        }, 1000 / 60);
+   
     }
 }
