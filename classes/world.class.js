9868db5cbbd2f2fd2e5b2bd990475ea48671d9df
Tobias Nölle <tobias.noelle@icloud.com>
2024-03-26 19:54:24 +0100

    added more layer images to expand the world

--- classes/world.class.js
+++ classes/world.class.js
@@ -1,9 +1,27 @@
 class World {
     layers = [
+        new LayerObject('./img/5_background/layers/air.png', -719),
+        new LayerObject('./img/5_background/layers/3_third_layer/2.png', -719),
+        new LayerObject('./img/5_background/layers/2_second_layer/2.png', -719),
+        new LayerObject('./img/5_background/layers/1_first_layer/2.png', -719),
+
         new LayerObject('./img/5_background/layers/air.png', 0),
         new LayerObject('./img/5_background/layers/3_third_layer/1.png', 0),
         new LayerObject('./img/5_background/layers/2_second_layer/1.png', 0),
-        new LayerObject('./img/5_background/layers/1_first_layer/1.png', 0)
+        new LayerObject('./img/5_background/layers/1_first_layer/1.png', 0),
+        new LayerObject('./img/5_background/layers/air.png', 719),
+        new LayerObject('./img/5_background/layers/3_third_layer/2.png', 719),
+        new LayerObject('./img/5_background/layers/2_second_layer/2.png', 719),
+        new LayerObject('./img/5_background/layers/1_first_layer/2.png', 719),
+
+        new LayerObject('./img/5_background/layers/air.png', 719 * 2),
+        new LayerObject('./img/5_background/layers/3_third_layer/1.png', 719 * 2),
+        new LayerObject('./img/5_background/layers/2_second_layer/1.png', 719 * 2),
+        new LayerObject('./img/5_background/layers/1_first_layer/1.png', 719 * 2),
+        new LayerObject('./img/5_background/layers/air.png', 719 * 3),
+        new LayerObject('./img/5_background/layers/3_third_layer/2.png', 719 * 3),
+        new LayerObject('./img/5_background/layers/2_second_layer/2.png', 719 * 3),
+        new LayerObject('./img/5_background/layers/1_first_layer/2.png', 719 * 3)
     ];
     character = new Character();
     chickens = [
