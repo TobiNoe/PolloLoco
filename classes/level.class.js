2a1afb98ae4c795105158a6fc482baba8e568da0
Tobias Nölle <tobias.noelle@icloud.com>
2024-03-27 20:16:26 +0100

    added level.class.js and level1.js

---
+++ classes/level.class.js
@@ -0,0 +1,11 @@
+class Level {
+    layers;
+    chickens;
+    clouds;
+    
+    constructor (layers, chickens, clouds) {
+        this.layers = layers;
+        this.chickens = chickens;
+        this.clouds = clouds;
+    }
+}
--- classes/world.class.js
+++ classes/world.class.js
@@ -1,38 +1,8 @@
 class World {
-    layers = [
-        new LayerObject('./img/5_background/layers/air.png', -719),
-        new LayerObject('./img/5_background/layers/3_third_layer/2.png', -719),
-        new LayerObject('./img/5_background/layers/2_second_layer/2.png', -719),
-        new LayerObject('./img/5_background/layers/1_first_layer/2.png', -719),
-
-        new LayerObject('./img/5_background/layers/air.png', 0),
-        new LayerObject('./img/5_background/layers/3_third_layer/1.png', 0),
-        new LayerObject('./img/5_background/layers/2_second_layer/1.png', 0),
-        new LayerObject('./img/5_background/layers/1_first_layer/1.png', 0),
-        new LayerObject('./img/5_background/layers/air.png', 719),
-        new LayerObject('./img/5_background/layers/3_third_layer/2.png', 719),
-        new LayerObject('./img/5_background/layers/2_second_layer/2.png', 719),
-        new LayerObject('./img/5_background/layers/1_first_layer/2.png', 719),
-
-        new LayerObject('./img/5_background/layers/air.png', 719 * 2),
-        new LayerObject('./img/5_background/layers/3_third_layer/1.png', 719 * 2),
-        new LayerObject('./img/5_background/layers/2_second_layer/1.png', 719 * 2),
-        new LayerObject('./img/5_background/layers/1_first_layer/1.png', 719 * 2),
-        new LayerObject('./img/5_background/layers/air.png', 719 * 3),
-        new LayerObject('./img/5_background/layers/3_third_layer/2.png', 719 * 3),
-        new LayerObject('./img/5_background/layers/2_second_layer/2.png', 719 * 3),
-        new LayerObject('./img/5_background/layers/1_first_layer/2.png', 719 * 3)
-    ];
     character = new Character();
-    chickens = [
-        new Chicken(),
-        new Chicken(),
-        new Chicken()
-    ];
-    clouds = [
-        new Cloud(),
-        new Cloud()
-    ];
+    layers = level1.layers;
+    chickens = level1.chickens ;
+    clouds = level1.clouds;
     canvas;
     ctx;
     keyboard;
