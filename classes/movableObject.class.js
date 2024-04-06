5b8d438ef592a1d2a7f10b67d686070e699dbf66
Tobias Nölle <tobias.noelle@icloud.com>
2024-04-06 17:25:24 +0200

    added function draw() and drawFrame() into movableObject

--- classes/movableObject.class.js
+++ classes/movableObject.class.js
@@ -62,4 +62,16 @@ class MovableObject {
     jump() {
         this.speedY = 16;
     }
+
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+    drawFrame(ctx) {
+        ctx.beginPath();
+        ctx.lineWidth = '4';
+        ctx.strokeStyle = 'green';
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.stroke();
+    }
 }
--- classes/world.class.js
+++ classes/world.class.js
@@ -28,7 +28,7 @@ class World {
         this.drawObjectsIntoMap(this.level.clouds);
         this.drawObjectsIntoMap(this.level.items);
         this.drawObjectsIntoMap(this.level.enemies);
-        
+
         this.drawIntoMap(this.character);
 
         this.ctx.translate(-this.cameraX, 0);
@@ -39,10 +39,10 @@ class World {
         });
     }
 
-    drawObjectsIntoMap(arr){
+    drawObjectsIntoMap(arr) {
         arr.forEach(obj => {
             this.drawIntoMap(obj);
-        });   
+        });
     }
 
     drawIntoMap(obj) {
@@ -51,10 +51,21 @@ class World {
             this.ctx.save(); // Einstellungen werden gespeichert
             this.ctx.translate(obj.width, 0); // Der Translate-Befehl setzt die gedrehte Position des Objektes neu, indem die Breite des Objektes abgezogen wird
             this.ctx.scale(-1, 1); // Das Pbjekt wird negiert (gedreht) in den Canvas gelade (scaliert)
-            obj.x = obj.x * -1; //Die x-Koordinate des Objektes muß einmal negiert werden, weil die X-Achse im Cnavas umgedfreht wird (0 ist jetzt Rechts)
+            obj.x = obj.x * -1; //Die x-Koordinate des Objektes muß einmal negiert werden, weil die X-Achse im Cnavas umgedreht wird (0 ist jetzt Rechts)
         }
-        this.ctx.drawImage(obj.img, obj.x, obj.y, obj.width, obj.height);
-        /*  otherDirection = true / Taste nach Links wurde gedrückt  */
+
+        //draw Object
+        obj.draw(this.ctx);
+
+        //draw rectangle
+        obj.drawFrame(this.ctx);
+        this.ctx.beginPath();
+        this.ctx.lineWidth = "4";
+        this.ctx.strokeStyle = "green";
+        this.ctx.rect(obj.x, obj.y, obj.width, obj.height);
+        this.ctx.stroke();
+
+        /*  otherDirection = false / Taste nach Links wurde nicht gedrückt  */
         if (obj.otherDirection) {
             this.ctx.restore(); // Normale Einstellungen des Canvas werden wieder geladen
             obj.x = obj.x * -1; // X-Achse wird wieder zurück gedreht (0 ist wieder Links)
