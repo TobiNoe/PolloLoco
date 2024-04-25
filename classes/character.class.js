451aec8c242b4bb2298febb37ca500db1680e621
Tobias Nölle <tobias.noelle@icloud.com>
2024-04-25 19:45:08 +0200

    feat: integrate imagesIdle and change animate function into character.class.js. Instance of character.class.js play animation Idle if nothing other event is happening

--- classes/character.class.js
+++ classes/character.class.js
@@ -48,12 +48,36 @@ class Character extends MovableObject {
         './img/2_character_pepe/4_hurt/H-42.png',
         './img/2_character_pepe/4_hurt/H-43.png'
     ];
+    imagesIdle = [
+        './img/2_character_pepe/1_idle/idle/I-1.png',
+        './img/2_character_pepe/1_idle/idle/I-2.png',
+        './img/2_character_pepe/1_idle/idle/I-3.png',
+        './img/2_character_pepe/1_idle/idle/I-4.png',
+        './img/2_character_pepe/1_idle/idle/I-5.png',
+        './img/2_character_pepe/1_idle/idle/I-6.png',
+        './img/2_character_pepe/1_idle/idle/I-7.png',
+        './img/2_character_pepe/1_idle/idle/I-8.png',
+        './img/2_character_pepe/1_idle/idle/I-9.png',
+        './img/2_character_pepe/1_idle/idle/I-10.png',
+        './img/2_character_pepe/1_idle/long_idle/I-11.png',
+        './img/2_character_pepe/1_idle/long_idle/I-12.png',
+        './img/2_character_pepe/1_idle/long_idle/I-13.png',
+        './img/2_character_pepe/1_idle/long_idle/I-14.png',
+        './img/2_character_pepe/1_idle/long_idle/I-15.png',
+        './img/2_character_pepe/1_idle/long_idle/I-16.png',
+        './img/2_character_pepe/1_idle/long_idle/I-17.png',
+        './img/2_character_pepe/1_idle/long_idle/I-18.png',
+        './img/2_character_pepe/1_idle/long_idle/I-19.png',
+        './img/2_character_pepe/1_idle/long_idle/I-20.png'
+    ];
+    isIdle = false;
     world;
     speed = 1; // speed default 1
     walkingSound = new Audio('../audio/walking.mp3');
 
     constructor() {
         super().loadImage('./img/2_character_pepe/1_idle/idle/I-1.png');
+        this.loadImages(this.imagesIdle);
         this.loadImages(this.imagesWalking);
         this.loadImages(this.imagesJumping);
         this.loadImages(this.imagesDead);
@@ -112,8 +136,10 @@ class Character extends MovableObject {
                 } else {
                     if (this.world.keyboard.right || this.world.keyboard.left) {
                         this.playAnimation(this.imagesWalking);
+                    } else if (this.isIdle) {
+                        this.playAnimation(this.imagesIdle);
                     } else {
-                        this.loadImage('./img/2_character_pepe/1_idle/idle/I-1.png');
+                        this.loadImage('./img/2_character_pepe/1_idle/idle/I-1.png');   
                     }
                 }
             }
