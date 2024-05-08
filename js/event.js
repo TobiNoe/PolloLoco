5f6b0c9b29c00d92af539795fbd0a60c717e5873
Tobias Nölle <tobias.noelle@icloud.com>
2024-05-08 19:57:10 +0200

    refactor: event.js

--- js/event.js
+++ js/event.js
@@ -48,25 +48,21 @@ window.addEventListener('keyup', (e) => {
 document.getElementById('btnLeft').addEventListener('touchstart', (e) => {
     e.preventDefault();
     keyboard.left = true;
-    console.log(keyboard.left);
 });
 
 document.getElementById('btnLeft').addEventListener('touchend', (e) => {
     e.preventDefault();
     keyboard.left = false;
-    console.log(keyboard.left);
 });
 
 document.getElementById('btnRight').addEventListener('touchstart', (e) => {
     e.preventDefault();
     keyboard.right = true;
-    console.log(keyboard.right);
 });
 
 document.getElementById('btnRight').addEventListener('touchend', (e) => {
     e.preventDefault();
     keyboard.right = false;
-    console.log(keyboard.right);
 });
 
 document.getElementById('btnThrow').addEventListener('touchstart', (e) => {
