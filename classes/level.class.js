936de6243f232b9e9ec8d03c4b82db97da940907
Tobias Nölle <tobias.noelle@icloud.com>
2024-05-15 19:47:06 +0200

    docs: level class

--- classes/level.class.js
+++ classes/level.class.js
@@ -4,8 +4,15 @@ class Level {
     clouds;
     items;
     levelEndX = 2400;
-    
-    constructor (layers, enemies, clouds, items) {
+
+    /**
+     * Creates an instance of Level.
+     * @param {Layer[]} layers - The layers of the level.
+     * @param {Enemy[]} enemies - The enemies in the level.
+     * @param {Cloud[]} clouds - The clouds in the level.
+     * @param {Item[]} items - The items in the level.
+     */
+    constructor(layers, enemies, clouds, items) {
         this.layers = layers;
         this.enemies = enemies;
         this.clouds = clouds;
