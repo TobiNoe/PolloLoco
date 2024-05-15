ee21c4312e9980ca227dea5e62d133ad3d8ff99d
Tobias Nölle <tobias.noelle@icloud.com>
2024-05-15 19:44:59 +0200

    docs: layerObject class

--- classes/layerObject.class.js
+++ classes/layerObject.class.js
@@ -1,12 +1,15 @@
 class LayerObject extends MovableObject {
     width = 720;
     height = 480;
-    
-    
-    constructor(src, x){
+
+    /**
+    * Creates an instance of LayerObject.
+    * @param {string} src - The image source path for the LayerObject.
+    * @param {number} x - The x position of the LayerObject.
+    */
+    constructor(src, x) {
         super().loadImage(src);
         this.x = x;
-        //man kann die y-koordinate berechenen. 480 ist die Höhe des canvas - die Höhe des Objektes
         this.y = 480 - this.height;
     }
 }
