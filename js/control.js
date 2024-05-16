4d0bcf9123aba1451f868fe7d261deb1245516ae
Tobias Nölle <tobias.noelle@icloud.com>
2024-05-16 21:39:47 +0200

    docs: control.js

--- js/control.js
+++ js/control.js
@@ -4,10 +4,10 @@ let audioElements = [];
 let mute = false;
 
 /**
- * Sets up a stoppable interval.
- * @param {Function} fn - The function to execute at each interval.
- * @param {number} time - The interval time in milliseconds.
- */
+* Sets up a stoppable interval.
+* @param {Function} fn - The function to execute at each interval.
+* @param {number} time - The interval time in milliseconds.
+*/
 function setStoppableInterval(fn, time) {
     let options = {
         fnName: fn,
@@ -18,12 +18,21 @@ function setStoppableInterval(fn, time) {
     intervalFN.push(options);
 }
 
+/**
+* @param {string} audioPath
+* @returns {Audio} The Audio object added to the audio elements.
+* @description Creates a mutable Audio object and adds it to the audio elements.
+*/
 function setMutableAudio(audioPath) {
     let audio = new Audio(audioPath);
     audioElements.push(audio);
     return audio;
 }
 
+/**
+* @returns {void}
+* @description Mutes all the audio elements.
+*/
 function stopAudio() {
     mute = true;
     audioElements.forEach(audio => {
@@ -31,6 +40,10 @@ function stopAudio() {
     });
 }
 
+/**
+* @returns {void}
+* @description Unmutes all the audio elements.
+*/
 function startAudio() {
     mute = false;
     audioElements.forEach(audio => {
@@ -38,34 +51,47 @@ function startAudio() {
     });
 }
 
+/**
+* @returns {void}
+* @description Mutes the audio and changes the sound button icon.
+*/
 function muteAudio() {
     toggleIcons('btnSoundOn', 'btnSoundOff');
     stopAudio();
 }
 
+/**
+* @returns {void}
+* @description Unmutes the audio and changes the sound button icon.
+*/
 function unmuteAudio() {
     toggleIcons('btnSoundOff', 'btnSoundOn');
     startAudio();
 }
 
+/**
+* @param {Audio} soundFile
+* @returns {Audio} The provided Audio object if it is muted.
+* @description Checks if the audio is muted and returns the sound file.
+*/
 function checkMuteOn(soundFile) {
     if (audioElements[0].volume === 0) {
         soundFile.volume = 0;
         console.log(soundFile);
-        return soundFile;   
+        return soundFile;
     }
 }
 
 /**
- * Stops all running intervals.
- */
+* Stops all running intervals.
+*/
 function stopGame() {
     intervalIDs.forEach(clearInterval);
 }
 
 /**
- * Restarts all intervals that were previously stopped.
- */
+* Restarts all intervals that were previously stopped.
+*/
 function restartGame() {
     let count = intervalIDs.length;
     intervalIDs.forEach(index => {
@@ -76,12 +102,18 @@ function restartGame() {
     intervalIDs.splice(0, count);
 }
 
+/**
+* The element to be set to full screen mode.
+*/
 function fullScreen() {
     let fullscreen = document.getElementById('game');
     enterFullscreen(fullscreen);
     toggleIcons('btnFullScreen', 'btnNoFullScreen');
 }
 
+/**
+* Requests the specified element to enter full screen mode.
+*/
 function enterFullscreen(element) {
     if (element.requestFullscreen) {
         element.requestFullscreen();
@@ -92,6 +124,9 @@ function enterFullscreen(element) {
     }
 }
 
+/**
+* Exits full screen mode.
+*/
 function exitFullscreen() {
     if (document.exitFullscreen) {
         document.exitFullscreen();
@@ -100,6 +135,11 @@ function exitFullscreen() {
     }
 }
 
+/**
+* Toggles the icons of two elements.
+* @param {string} icon1 - The id of the first icon element.
+* @param {string} icon2 - The id of the second icon element.
+*/
 function toggleIcons(icon1, icon2) {
     document.getElementById(icon1).classList.add('d-none');
     document.getElementById(icon1).classList.remove('d-flex');
@@ -107,6 +147,9 @@ function toggleIcons(icon1, icon2) {
     document.getElementById(icon2).classList.add('d-flex');
 }
 
+/**
+* Shows or hides the mobile control pad based on the user agent.
+*/
 function showMobileControlPad() {
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         document.getElementById('mobileControl').classList.add('d-flex');
@@ -115,12 +158,18 @@ function showMobileControlPad() {
     }
 }
 
+/**
+* Shows the information element and stops the game.
+*/
 function showInfo() {
     document.getElementById('info').classList.remove('d-none');
     document.getElementById('info').classList.add('d-flex');
     stopGame();
 }
 
+/**
+* Hides the information panel.
+*/
 function hideInfo() {
     document.getElementById('info').classList.add('d-none');
     document.getElementById('info').classList.remove('d-flex');
