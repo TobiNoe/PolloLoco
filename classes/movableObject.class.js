9cb0c201b04addb534126936770f1c793d9ac07d
Tobias Nölle <tobias.noelle@icloud.com>
2024-05-18 12:12:46 +0200

    refactor and docs: movableObject class

--- classes/movableObject.class.js
+++ classes/movableObject.class.js
@@ -26,23 +26,44 @@ class MovableObject extends DrawableObject {
      * Simulates gravity by applying a downward force to the object.
      */
     applyGravity() {
-        setStoppableInterval(() => {
-            if (this.isAboveGround() || this.speedY > 0) {
-                this.y -= this.speedY;
-                this.speedY -= this.acceleration;
-            }
-        }, 1000 / 30);
+        setStoppableInterval(() => this.jumpingFly(), 1000 / 30);
     }
 
+    /**
+     * Makes the object fly upwards if it is above the ground or currently flying.
+     * Updates the y-coordinate and speed of the object based on its acceleration.
+     */
+    jumpingFly() {
+        if (this.isAboveGround() || this.isFlying()) {
+            this.y -= this.speedY;
+            this.speedY -= this.acceleration;
+        }
+    }
+
+    /**
+     * Checks if the object is flying.
+     * @returns {boolean} True if the object's vertical speed (speedY) is greater than 0, otherwise false.
+     */
+    isFlying() {
+        return this.speedY > 0;
+    }
+
+    /**
+     * Checks if the object is above the ground.
+     * @returns {boolean} True if the object is above the ground; always true for throwable objects, or if the y-coordinate is less than 178 for other objects.
+     */
     isAboveGround() {
         if (this instanceof ThrowableObject) {
             return true;
         } else {
             return this.y < 178;
         }
-
     }
 
+    /**
+     * Moves the object to the right if it is not dead.
+     * Updates the x-coordinate of the object and sets the direction flag.
+     */
     moveRight() {
         if (!this.isDead()) {
             this.x += this.speed;
@@ -50,20 +71,33 @@ class MovableObject extends DrawableObject {
         }
     }
 
+    /**
+     * Moves the object to the left if it is not dead.
+     * Updates the x-coordinate of the object.
+     */
     moveLeft() {
         if (!this.isDead()) {
             this.x -= this.speed;
         }
     }
 
+    /**
+     * Plays the specified animation for the object.
+     * Cycles through the provided images and updates the object's image.
+     * @param {Array} images - An array of image paths for the animation.
+     */
     playAnimation(images) {
         let i = this.currentImage % images.length;
-        // Modulo Operation speichert immer den rest 0,1,2,3,4,5,0,1....
         let path = images[i];
         this.img = this.imgCache[path];
         this.currentImage++;
     }
 
+    /**
+     * Plays the death animation for the object.
+     * Resets the current image if needed and plays the death animation sequence.
+     * @param {Array} images - An array of image paths for the death animation.
+     */
     playAnimationIsDead(images) {
         if (this.resetCurrentImage) {
             this.currentImage = 0;
@@ -79,6 +113,11 @@ class MovableObject extends DrawableObject {
         }
     }
 
+    /**
+     * Plays the jump animation for the object.
+     * Resets the current image if needed and plays the jump animation sequence.
+     * @param {Array} images - An array of image paths for the jump animation.
+     */
     playAnimationJump(images) {
         if (this.resetCurrentImage) {
             this.currentImage = 0;
@@ -89,47 +128,64 @@ class MovableObject extends DrawableObject {
             let path = images[i];
             this.img = this.imgCache[path];
             this.currentImage++;
-            if (i === 3) {
-                this.speedY = 20;
-                this.noMove = false;
-                this.jumpingSound.play();
-            } else if (i === 8) {
-                this.y = 178;
-                this.noMove = true;
-            }
-        } else if (this.currentImage = images.length) {
+            this.checkJumpAnimationMovement(i);
+        } else if (this.currentImage === images.length) {
             this.isJump = false;
             this.noMove = false;
             this.resetCurrentImage = true;
         }
     }
 
+    /**
+     * Checks and updates the movement of the object during the jump animation.
+     * @param {number} i - The current index of the jump animation.
+     */
+    checkJumpAnimationMovement(i) {
+        if (i === 3) {
+            this.speedY = 20;
+            this.noMove = false;
+            this.jumpingSound.play();
+        } else if (i === 8) {
+            this.y = 178;
+            this.noMove = true;
+        }
+    }
+
+    /**
+     * Initiates the jump action for the object.
+     */
     jump() {
         this.isJump = true;
     }
 
+    /**
+     * Checks if the object is colliding with another object.
+     * @param {Object} obj - The other object to check collision with.
+     * @returns {boolean} True if the object is colliding with the other object, otherwise false.
+     */
     isColliding(obj) {
-        // R -> L check collision Charater right side with obj left side //korrektur da Offset-Links addiert werden musste!!! und nicht Offset-Rechts
-        // L -> R check collision Charater left side with obj right side side
-        // T -> B check collision Charater ontop with obj
-        // B -> T check collision Charater bottom the obj
         return this.x + this.width - this.offset.right > obj.x + obj.offset.left &&
             this.x + this.offset.left < obj.x + obj.width - obj.offset.right &&
             this.y + this.height - this.offset.bottom > obj.y + obj.offset.top &&
-            this.y + this.offset.top < obj.y + obj.height - obj.offset.bottom
+            this.y + this.offset.top < obj.y + obj.height - obj.offset.bottom;
     }
 
+    /**
+     * Checks if the object is jumping on top of another object.
+     * @param {Object} obj - The other object to check for a jump on.
+     * @returns {boolean} True if the object is jumping on the other object, otherwise false.
+     */
     isJumpOn(obj) {
-        // R -> L check collision Charater right side with obj left side
-        // L -> R check collision Charater left side with obj right side side
-        // T -> B check collision Charater ontop with obj
-        // B -> T check collision Charater bottom the obj
         return this.x + this.width - this.offsetJumpOn.right > obj.x + obj.offset.right &&
             this.x + this.offsetJumpOn.left < obj.x + obj.width - obj.offset.right &&
             this.y + this.height - this.offsetJumpOn.bottom > obj.y + obj.offset.top &&
-            this.y + this.offsetJumpOn.top < obj.y + obj.height - obj.offset.bottom
+            this.y + this.offsetJumpOn.top < obj.y + obj.height - obj.offset.bottom;
     }
 
+    /**
+     * Reduces the object's energy when it is hit.
+     * If the energy drops below zero, it is set to zero. Updates the last hit time.
+     */
     hit() {
         this.energy -= 0.5;
         if (this.energy < 0) {
@@ -139,13 +195,22 @@ class MovableObject extends DrawableObject {
         }
     }
 
+    /**
+     * Checks if the object is currently hurt.
+     * @returns {boolean} True if the object was hit within the last 0.5 seconds, otherwise false.
+     */
     isHurt() {
-        let timePassed = new Date().getTime() - this.lastHit; // Difference in ms
+        let timePassed = new Date().getTime() - this.lastHit;
         timePassed = timePassed / 1000;
         return timePassed < 0.5;
     }
 
+    /**
+     * Checks if the object is dead.
+     * @returns {boolean} True if the object's energy is zero, otherwise false.
+     */
     isDead() {
         return this.energy === 0;
     }
+
 }
